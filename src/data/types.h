028e41d74f0dda66297ed6dfaa7c33f07d6c14c2
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2024-06-12 21:24:00 +0300

    Fix static typo on types

--- src/data/types.h
+++ src/data/types.h
@@ -2,8 +2,8 @@
 
 #include "dbdata.h"
 
-static ValueType* VT_INT;
-static ValueType* VT_BOOL;
-static ValueType* VT_TEXT_30;
+ValueType* VT_INT;
+ValueType* VT_BOOL;
+ValueType* VT_TEXT_30;
 
 void init_vtypes();
